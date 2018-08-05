0b5ddf999448f3f15ee6243598febd87f0314061
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 16:23:14 +0000

    automatically backed up by learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -3,9 +3,11 @@ class PostSerializer
 
     serialized_product = '{'
 
-    serialized_product += '"id": ' + post.id.to_s + ', '
-    serialized_product += '"title": "' + post.title + '", '
-    serialized_product += '"description": "' + post.description + '", '
+    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"price": "' + product.price + '", '
+    serialized_product += '"inventory": "' + product.inventory + '", '
+    serialized_product += '"description": "' + product.description + '", '
 
     serialized_product += '}'
   end
